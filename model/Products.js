237612239bb9e7e014389df864949a652f2d396f
Milton Chandradas <milton.chandradas@sap.com>
2020-03-19 01:46:33 -0400

    Initial commit

---
+++ model/Products.js
@@ -0,0 +1,60 @@
+const products = [
+   {
+      _id: 1,
+      price: 9201,
+      color: "red",
+      type: "T-Shirt",
+      gender: "female",
+      title: "sit anim Lorem",
+      company: "pantaloons",
+      about:
+         "Mollit ut qui duis qui duis. Incididunt commodo aliquip nisi fugiat consequat minim ut non irure occaecat do. Reprehenderit fugiat eu duis dolore veniam reprehenderit incididunt anim eu ullamco ipsum aute magna aliquip. Consectetur anim qui sunt cupidatat incididunt esse tempor. Eiusmod ut quis cupidatat do esse esse officia.\r\n"
+   },
+   {
+      _id: 2,
+      price: 9887,
+      color: "white",
+      type: "Saree",
+      gender: "female",
+      title: "enim in Lorem",
+      company: "W",
+      about:
+         "Et sit laboris duis irure minim aliquip sint ut aliqua velit consectetur magna nisi reprehenderit. Aute sunt dolore excepteur et adipisicing nostrud cillum sint eu amet voluptate in officia ut. Reprehenderit incididunt eu velit ea reprehenderit reprehenderit. Consequat esse fugiat occaecat est adipisicing exercitation. Laborum aliqua irure aliqua ipsum ut magna. Ad ea nulla ipsum quis consequat deserunt id.\r\n"
+   },
+   {
+      _id: 3,
+      price: 6471,
+      color: "white",
+      type: "Pant",
+      gender: "female",
+      title: "sunt esse Lorem",
+      company: "Benetton",
+      about:
+         "Eiusmod consectetur velit do nisi. In in reprehenderit ut Lorem do duis et sunt. Minim laboris aliquip fugiat sint tempor mollit qui qui commodo excepteur ullamco magna amet. Occaecat voluptate sit irure commodo sunt sint. Enim est et in excepteur nulla reprehenderit reprehenderit duis exercitation. Incididunt laborum adipisicing amet minim est sit ullamco consectetur. Consectetur irure consequat duis nulla duis laborum magna ex ut Lorem occaecat irure dolor.\r\n"
+   },
+   {
+      _id: 4,
+      price: 2533,
+      color: "red",
+      type: "Jumpsuit",
+      gender: "female",
+      title: "id consectetur eiusmod",
+      company: "silkHouse",
+      about:
+         "Sunt fugiat culpa labore voluptate ex sunt. Id excepteur commodo in qui ea ad commodo. Labore elit elit esse minim nulla. Occaecat sunt consectetur aliquip occaecat excepteur in labore laboris mollit adipisicing occaecat veniam. Lorem excepteur cillum ea nisi magna id cillum irure.\r\n"
+   },
+   {
+      _id: 5,
+      price: 4522,
+      color: "white",
+      type: "Leggings",
+      gender: "female",
+      title: "excepteur ipsum sit",
+      company: "pantaloons",
+      about:
+         "Excepteur reprehenderit consectetur anim et amet commodo ullamco adipisicing aliqua. Exercitation pariatur laborum velit deserunt magna ullamco amet irure labore laborum. Consequat anim ipsum labore eu minim ea. Officia dolor est mollit sint ad consequat.\r\n"
+   }
+];
+
+
+module.exports = products;
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+const products = require("./routes/products");
+
 // Load environment variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
+// Mount routers
+app.use("/api/v1/products", products);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-   PORT,
-   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
